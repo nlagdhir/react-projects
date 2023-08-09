54ee492ad900b23d8bbb8bae1f29ab4c2d2fcb60
Nilesh Lagdhir <nlagdhir@gmail.com>
2023-08-09 18:44:02 +0530

    props destructuring

--- booklist-app/src/index.js
+++ booklist-app/src/index.js
@@ -3,28 +3,46 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const firstBook = {
+  author: "Jordan Moore",
+  title: "Interesting Facts For Curious Minds",
+  img: "./../images/book1.jpg",
+};
+
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const author = 'Jordan Moore';
-const Book = () => {
-  const title = 'Interesting Facts For Curious Minds';
+const Book = (props) => {
+  // destructuring props in function parameters
+
+  const { img, title, author, children } = props;
   return (
     <article className="book">
-      <img
-        src="./../images/book1.jpg"
-        alt="Interesting Facts For Curious Minds"
-      />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };
