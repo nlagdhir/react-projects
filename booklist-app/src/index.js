86d1050e3ffb0fd1bf1a748141b8eef880b417c7
Nilesh Lagdhir <nlagdhir@gmail.com>
2023-08-10 12:21:31 +0530

    props drilling and pass props to down component

--- booklist-app/src/index.js
+++ booklist-app/src/index.js
@@ -19,44 +19,28 @@ const books = [
 ];
 
 const BookList = () => {
+  const someValue = 'Some Value';
+  const displayValue = () => {
+    console.log(someValue);
+  }
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map((book) => {
-        return <Book key={book.id} {...book} />;
+        return <Book key={book.id} {...book} displayValue={displayValue} />;
       })}
-    </section>
-  );
-};
-
-const EventExamples = () => {
-  return (
-    <section>
-      <form>
-        <h2>Event</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={(e) => console.log(e.target.value)}
-          style={{ margin: "1erm 0" }}
-        />
-        <button type="submit">Submit</button>
-        <button type="button" onClick={() => console.log("button clicked!")}>
-          Click me
-        </button>
-      </form>
     </section>
   );
 };
 
 const Book = (props) => {
   // destructuring props in function parameters
-  const { img, title, author, children } = props;
+  const { img, title, author, children, displayValue } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button onClick={displayValue}>Display Title</button>
       {children}
     </article>
   );
