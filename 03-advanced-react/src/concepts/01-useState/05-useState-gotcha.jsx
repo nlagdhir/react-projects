bd78667a5a0a9574600b00eb9eba959cf0dc25f6
Nilesh Lagdhir <nlagdhir@gmail.com>
2023-08-25 18:32:37 +0530

    useState gotcha and worked with prevState and setTimeout function

--- 03-advanced-react/src/concepts/01-useState/05-useState-gotcha.jsx
+++ 03-advanced-react/src/concepts/01-useState/05-useState-gotcha.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setTimeout(() => {
+      console.log("button clicked!"); 
+      // setValue(value + 1); won't work as it won't get latest value
+      setValue((prevValue) => {
+        return prevValue + 1;
+      })
+    },3000);
+  };
+
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Increase Value
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
