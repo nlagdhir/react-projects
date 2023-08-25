9e7f5dc58f4a3d4004641f415c5feb9c444ebd8b
Nilesh Lagdhir <nlagdhir@gmail.com>
2023-08-25 17:01:09 +0530

    worked with useState and Array

--- 03-advanced-react/src/concepts/01-useState/03-useState-array.jsx
+++ 03-advanced-react/src/concepts/01-useState/03-useState-array.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { data } from "../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  console.log(people);
+
+  const handleRemoveItem = (id) => {
+    setPeople(people.filter((people) => people.id !== id));
+  };
+
+  const handleRemoveAllItem = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div className="item" key={id}>
+            <h2>{name}</h2>
+            <button onClick={() => handleRemoveItem(id)}>Remove</button>
+          </div>
+        );
+      })}
+      <button
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={() => handleRemoveAllItem()}
+      >
+        Remove All item
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
