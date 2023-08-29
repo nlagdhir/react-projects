5102463b45d3fd18482be9747c2f6c7b09c8d191
Nilesh Lagdhir <nlagdhir@gmail.com>
2023-08-29 16:37:47 +0530

    birthday buddy app

---
+++ 04-fundamental-projects/01-birthday-buddy/src/App.jsx
@@ -0,0 +1,20 @@
+import data from "../src/data";
+import { useState } from "react";
+import List from "./List";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday Today</h3>
+        <List people={people} /> 
+        <button type="button" className="btn btn-block" onClick={() => setPeople([])}>
+          {" "}
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
+};
+export default App;
---
+++ 04-fundamental-projects/01-birthday-buddy/src/List.jsx
@@ -0,0 +1,13 @@
+import Person from "./Person";
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </>
+  );
+};
+
+export default List;
